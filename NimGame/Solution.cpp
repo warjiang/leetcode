77af8d873446f480dd2d1b9eace4085ffa44e17d
warjiang <1096409085@qq.com>
2017-02-27 09:02:37 +0800

    add Nim Game

---
+++ NimGame/Solution.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	bool canWinNim(int n) {
+        return n%4 != 0 ? true : false;
+    }
+};
---
+++ NimGame/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include "Solution.cpp"
+using namespace std;
+
+void tranverseVector(vector<int> v){
+	for(int i = 0; i < v.size(); i++){
+		cout << v[i] << endl;
+	}
+}
+
+int main(){
+	Solution* s = new Solution;
+	cout << s->canWinNim(6) << endl;
+	
+	//tranverseVector(v);
+	return 0;
+}
